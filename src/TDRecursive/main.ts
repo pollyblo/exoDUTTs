6d6dcbde2f835c3c0e61ce77aa29425bcb704957
pollyblo <ugzcollet@gmail.com>
2022-11-23 10:06:11 +0100

    tdRecursive

--- src/TDRecursive/main.ts
+++ src/TDRecursive/main.ts
@@ -1,12 +1,36 @@
-function copie(array: number[]): number[] {
-  let nouveau: number[] = [];
-  for (let i = 0; i < array.length; i++) {
-    nouveau.push(array[i]);
+function fibonacci(n: number): number {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
   }
-  return nouveau;
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-let array = [0, 2, 5, 6, 8];
-let array2 = [...array];
+function rechercheDichotomique(
+  array: number[],
+  guessedNumber: number,
+  a: number,
+  b: number
+): boolean {
+  console.log(a, b);
+  let m = Math.floor((a + b - 1) / 2);
+  console.log(m);
+  if (a >= b) {
+    return false;
+  } else if (a < b && guessedNumber === array[m]) {
+    console.log();
+    return true;
+  } else if (guessedNumber < array[m]) {
+    return rechercheDichotomique(array, guessedNumber, a, m);
+  } else {
+    return rechercheDichotomique(array, guessedNumber, m + 1, b);
+  }
+}
 
-console.log(array, array2);
+let array = [2, 5, 56, 60, 64, 68, 80];
+
+console.log(rechercheDichotomique(array, 80, 0, array.length));
+
+function palindrome(mot: string): boolean {}
